4975b0911c93b15fcfcde0c20f3fc94bb4475c97
Chameleon Cloud User <mingqiwangcn@gmail.com>
2021-08-19 14:10:16 -0500

    add OpenTableQA baseline data

--- gen_csv_log.py
+++ gen_csv_log.py
@@ -45,6 +45,18 @@ def list2str(data_lst):
     data_str = '\n'.join([str(a) for a in data_lst])
     return data_str
 
+def read_tapas_retrieval(mode):
+    data_file = '/home/cc/code/tapas/models/tapas_nq_hn_retriever_large/%s_knn_small.jsonl' % mode
+    ret_dict = {}
+    with open(data_file) as f:
+        for line in f:
+            item = json.loads(line)
+            qid = item['query_id']
+            table_info_lst = item['table_scores'][:5]
+            top_table_id_lst = [a['table_id'] for a in table_info_lst]
+            ret_dict[qid] = top_table_id_lst
+    return ret_dict
+
 def main():
     args = get_args()
     set_logger(args)
@@ -59,10 +71,12 @@ def main():
     out_file = os.path.join(args.out_dir, 'graph_preds_log_%s.csv' % args.mode)
     f_o = open(out_file, 'w')
     writer = csv.writer(f_o, delimiter='\t')
-    col_name_lst = ['qid', 'question', 'order', 'passage', 'gold (retrieved) table', 'table correct ?', 'answers']
+    col_name_lst = ['qid', 'question', 'order', 'passage', 'gold (retrieved) table', 'OpenTableQA table', 'table correct ?', 'answers']
     writer.writerow(col_name_lst)
 
     qry_result = read_qry_result(args.mode)
+
+    tapas_ret = read_tapas_retrieval(args.mode)
 
     out_script_file = './output/cp_ref_tables.sh'
     f_o_script = open(out_script_file, 'w')
@@ -89,6 +103,7 @@ def main():
             '',
             list2str(query_info['table_id_lst']),
             '',
+            '',
             list2str(query_info['answers'])
         ]
         writer.writerow(csv_q_info)
@@ -102,7 +117,8 @@ def main():
             if '/' in ref_table_id:
                 ref_table_id = ref_table_id.replace('/', '[left-slash]')
             f_o_script.write('cp ./tables_csv/"%s.csv" ./tables_ref \n' % ref_table_id)
-         
+        
+        tapas_table_id_lst = tapas_ret[query_info['qid']] 
         for idx, passage in enumerate(passage_lst):
             table_correct = ('Y' if passage_table_lst[idx] in gold_table_id_lst else '')
             csv_passage_info = [
@@ -111,6 +127,7 @@ def main():
                 (idx + 1),
                 passage,
                 passage_table_lst[idx],
+                tapas_table_id_lst[idx],
                 table_correct,
                 answer_lst[idx]['answer']
             ]
