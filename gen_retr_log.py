231ed50e7bc5128b64a5edb3e8aee23607083689
Chameleon Cloud User <mingqiwangcn@gmail.com>
2021-10-01 12:55:58 -0500

    add retr log

--- gen_retr_log.py
+++ gen_retr_log.py
@@ -19,21 +19,23 @@ def set_logger(args):
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--out_dir', type=str, default='./dataset/fetaqa/analysis_log')
+    parser.add_argument('--dataset', type=str)
+    parser.add_argument('--experiment', type=str)
+
     args = parser.parse_args()
     return args
 
-def get_questions():
+def get_questions(args):
     q_item_lst = []
-    qas_file = '/home/cc/data/FeTaQA/data/tf_records/interactions/dev_qas.jsonl'
+    qas_file = os.path.join('/home/cc/data', args.dataset, 'interactions/dev_qas.jsonl')
     with open(qas_file) as f:
         for line in f:
             q_item = json.loads(line)
             q_item_lst.append(q_item)
     return q_item_lst
 
-def read_qry_result():
-    data_file = './dataset/fetaqa/graph2txt_fabricqa/dev/preds_dev.json'
+def read_qry_result(args):
+    data_file = os.path.join('./dataset', args.dataset, args.experiment, 'dev/preds_dev.json')
     ret_dict = {}
     with open(data_file) as f:
         for line in f:
@@ -45,8 +47,8 @@ def list2str(data_lst):
     data_str = '\n'.join([str(a) for a in data_lst])
     return data_str
 
-def read_bm25_retrieval():
-    data_file = './dataset/fetaqa/graph2txt_bm25/dev/preds_dev.json'
+def read_bm25_retrieval(args):
+    data_file = os.path.join('./dataset', args.dataset, 'bm25_' + args.experiment, 'dev/preds_dev.json') 
     ret_dict = {}
     with open(data_file) as f:
         for line in f:
@@ -58,7 +60,12 @@ def read_bm25_retrieval():
 def main():
     args = get_args()
     set_logger(args)
-    query_info_lst = get_questions()
+    out_dir = os.path.join('dataset', args.dataset, args.experiment, 'dev/analysis_log')
+    if os.path.isdir(out_dir):
+        print('[%s] already exists\n' % out_dir)
+        return None
+    os.mkdir(out_dir)
+    query_info_lst = get_questions(args)
     query_info_dict = {}
     for query_info in query_info_lst:
         query_info_dict[query_info['qid']] = query_info 
@@ -66,7 +73,7 @@ def main():
     correct_retr_dict = {}
     for k in k_lst:
         correct_retr_dict[k] = []
-    out_file = os.path.join(args.out_dir, 'fetaqa_preds_log.csv')
+    out_file = os.path.join(out_dir, 'retr_log_%s_%s.csv' % (args.dataset, args.experiment))
     f_o = open(out_file, 'w')
     writer = csv.writer(f_o, delimiter='\t')
     col_name_lst = ['qid', 'question', 'order', 'fabricqa passage', 
@@ -74,10 +81,8 @@ def main():
                     'bm25 passage', 'bm25 table', 'bm25 correct?',
                     'found in top 100 of bm25']
     writer.writerow(col_name_lst)
-    qry_result = read_qry_result()
-    em25_ret = read_bm25_retrieval()
-    out_script_file = os.path.join(args.out_dir, 'fetaqa_cp_ref_tables.sh')
-    f_o_script = open(out_script_file, 'w')
+    qry_result = read_qry_result(args)
+    em25_ret = read_bm25_retrieval(args)
     log_qid_lst = []
     for query_info in tqdm(query_info_lst):
         batch_queries = [query_info] 
@@ -92,8 +97,8 @@ def main():
             q_correct_dict[k] = correct
             correct_retr_dict[k].append(correct)
         
-        #if q_correct_dict[1]: # ignore questions whose retrievaled top 1 table is correct. 
-        #    continue 
+        if q_correct_dict[1]: # ignore questions whose retrievaled top 1 table is correct. 
+            continue 
    
         found_top_100 = ('Y' if em25_ret[query_info['qid']]['found_top_100'] > 0 else 'N')
         log_qid_lst.append(query_info['qid'])    
@@ -115,16 +120,6 @@ def main():
         passage_lst = qry_ret['passages']
         passage_table_lst = qry_ret['passage_tags']
         answer_lst = qry_ret['answers']
-
-        all_ref_table_lst = gold_table_id_lst + passage_table_lst[:5]
-
-        for ref_table_id in all_ref_table_lst:
-            if '/' in ref_table_id:
-                ref_table_id = ref_table_id.replace('/', '[left-slash]')
-            if '"' in ref_table_id:
-                f_o_script.write("cp ./fetaqa_tables_csv/'%s.csv' ./fetaqa_tables_ref \n" % ref_table_id)
-            else: 
-                f_o_script.write('cp ./fetaqa_tables_csv/"%s.csv" ./fetaqa_tables_ref \n' % ref_table_id)
 
         top_em25_passages = em25_ret[query_info['qid']]['passages']
         top_em25_table_lst = em25_ret[query_info['qid']]['passage_tags']
@@ -151,14 +146,15 @@ def main():
         precision = np.mean(correct_retr_dict[k]) * 100
         logger.info('p@%d = %.2f' % (k, precision))
 
+    '''
     sample_qid_lst = random.sample(log_qid_lst, 100)
-    qid_file_name = os.path.join(args.out_dir, 'fetaqa_sample_qid_lst.txt')
+    qid_file_name = os.path.join(args.out_dir, 'examples.txt')
     with open(qid_file_name, 'w') as f_o_qid:
         for sample_qid in sample_qid_lst:
             f_o_qid.write(sample_qid + '\n')
+    '''
 
     f_o.close()
-    f_o_script.close()
 
 if __name__ == '__main__':
     main()
