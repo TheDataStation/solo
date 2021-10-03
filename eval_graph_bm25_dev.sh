9603b8e2bb9f9c9fe6795250030d96ea657e4ade
Chameleon Cloud User <mingqiwangcn@gmail.com>
2021-10-02 22:04:04 -0500

    update out dir

--- eval_graph_bm25_dev.sh
+++ eval_graph_bm25_dev.sh
@@ -1,4 +1,4 @@
 python eval_bm25.py \
---index_name nq_tables_graph_all_subject_class \
+--index_name nq_tables_graph_all_cols \
 --mode dev \
---out_dir ./dataset/nq_tables/bm25_graph_all_subject_class/dev
+--out_dir ./dataset/nq_tables/bm25_graph_all_cols/dev
