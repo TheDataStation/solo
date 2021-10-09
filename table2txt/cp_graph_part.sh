a8243efca56fd5ae84aeb783ddad2dbe1ba07020
Chameleon Cloud User <cc@data-1.novalocal>
2021-10-09 17:14:02 -0500

    update template dir

--- table2txt/cp_graph_part.sh
+++ table2txt/cp_graph_part.sh
@@ -8,7 +8,7 @@ part_name=part_$3
 expt_dir="/home/cc/code/plms_graph2text/webnlg/data/webnlg/${dataset}/${expt_name}"
 mkdir -p ${expt_dir}
 rm -rf ${expt_dir}/${part_name}
-cp -r ~/code/plms_graph2text/webnlg/data/webnlg/part_1 ${expt_dir}/${part_name}
+cp -r ~/code/plms_graph2text/webnlg/data/webnlg/template ${expt_dir}/${part_name}
 graph_parts_dir=./dataset/${dataset}/${expt_name}/graph_parts
 cp ${graph_parts_dir}/test_unseen.source_${part_name} ${expt_dir}/${part_name}/test_unseen.source
 cp ${graph_parts_dir}/test_unseen.target_${part_name} ${expt_dir}/${part_name}/test_unseen.target
