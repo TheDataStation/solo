5e8fa99b80040fb6f0e32a0ee7df747deef91360
Qiming <mingqiwangcn@gmail.com>
2021-10-17 23:25:12 -0500

    add question evaluation

--- table2txt/graph_strategy/main.py
+++ table2txt/graph_strategy/main.py
@@ -5,8 +5,14 @@ import argparse
 import shutil
 from table2txt.graph_strategy.strategy import Strategy
 from table2txt.graph_strategy.complete_graph import CompleteGraph
+from table2txt.graph_strategy.question_generator import QG
 import pytorch_lightning as pl
 from webnlg.finetune import main as generate_text_from_graph, SummarizationModule
+from fabric_qa.reader.forward_reader.rc_model import get_rc_model as get_reader
+from fabric_qa.reader.forward_reader.albert.qa_data import data_to_examples
+from tqdm import tqdm
+from fabric_qa import utils
+import numpy as np
 
 def read_tables(data_file):
     table_lst = []
@@ -38,14 +44,46 @@ def get_strategy_lst():
     stg_lst.append(stg_1)
     return stg_lst
 
-def evaluate_strategy(table, stg, args, graph_file_info):
+def evaluate_strategy(reader, qa_lst, table, stg, args, graph_file_info):
     generate_graph(table, stg, args, graph_file_info)
 
     args.output_dir = os.path.join(args.dataset_out_dir, stg.name) 
     generate_text_from_graph(args)
     
     table_passage_dict = read_passages(args, stg, graph_file_info)
+    table_id = table['tableId']
+    mean_f1 = evaluate_question(reader, qa_lst, table_id, table_passage_dict)    
     
+    print('(table=%s strategy=%s f1=%.2f)' % (table_id, stg.name, mean_f1 * 100))
+
+def evaluate_question(reader, qa_lst, table_id, table_passage_dict):
+    pred_f1_lst = []
+    for row, row_questions in enumerate(qa_lst):
+        pasage_key = '%s-%d' % (table_id, row)
+        row_passages = table_passage_dict[pasage_key]
+        passage_num = len(row_passages)
+        p_id_lst = [a for a in range(passage_num)]
+        for sub_idx, question_info in enumerate(row_questions):
+            q_p_batch = [{
+                'question':question_info['question'],
+                'passages':row_passages,
+                'qid':'%s-%d-%d' % (table_id, row, sub_idx),
+                'p_id_lst':p_id_lst
+            }]
+            batch_examples = data_to_examples(q_p_batch)
+            reader_out = reader(batch_examples)
+            preds = reader_out['preds']
+            answer_lst = []
+            for example in batch_examples:
+                item = preds[example.qas_id]
+                answer = item['text']
+                answer_lst.append(answer)
+            gold_answer = question_info['answer']
+            _, f1s = utils.compute_em_f1(answer_lst, [gold_answer])
+            best_f1 = max(f1s)
+            pred_f1_lst.append(best_f1)
+    mean_f1 = np.mean(pred_f1_lst)
+    return mean_f1 
 
 def read_meta_info(meta_file):
     meta_info_lst = []
@@ -94,7 +132,6 @@ def generate_graph(table, stg, args, graph_file_info):
     f_o_tgt.flush()
     f_o_meta.flush()
 
-
 def prepare_graph_file(stg, args):
     graph_folder = os.path.join(args.dataset_in_dir, stg.name)
     if os.path.exists(graph_folder):
@@ -120,7 +157,19 @@ def output_tables(table_lst):
         table_file = os.path.join(out_dir, table['tableId']+'.json')
         with open(table_file, 'w') as f_o:
             f_o.write(json.dumps(table))
-     
+
+def create_graph_file(stg_lst, args):
+    graph_file_info_lst = []
+    for stg in stg_lst:
+        graph_file_info = prepare_graph_file(stg, args)
+        graph_file_info_lst.append(graph_file_info)
+    return graph_file_info_lst
+
+def close_graph_file(graph_file_info_lst):
+    for graph_file_info in graph_file_info_lst:
+        graph_file_info['f_o_src'].close()
+        graph_file_info['f_o_tgt'].close()
+        graph_file_info['f_o_meta'].close()
  
 def main():
     random.seed(10)
@@ -130,13 +179,16 @@ def main():
 
     output_tables(table_lst)
 
-    for stg in stg_lst:
-        graph_file_info = prepare_graph_file(stg, args)
-        for table in table_lst:
-            evaluate_strategy(table, stg, args, graph_file_info)
-        graph_file_info['f_o_src'].close()
-        graph_file_info['f_o_tgt'].close()
-        graph_file_info['f_o_meta'].close()
+    reader = get_reader('albert', '/home/cc/code/fabric_qa/model/reader/forward_reader/model', 0)
+    q_generator = QG()
+    graph_file_info_lst = create_graph_file(stg_lst, args)
+    for table in table_lst:
+        qa_lst = q_generator.generate(table)
+        for stg_idx, stg in enumerate(stg_lst):
+            graph_file_info = graph_file_info_lst[stg_idx]
+            evaluate_strategy(reader, qa_lst, table, stg, args, graph_file_info)
+    
+    close_graph_file(graph_file_info_lst)        
 
 if __name__ == '__main__':
     main()
---
+++ table2txt/graph_strategy/question_generator.py
@@ -0,0 +1,64 @@
+from table2txt.graph_strategy.complete_graph import MAX_COL_SIZE, MAX_ENTITY_SIZE
+
+class QG:
+    def generate(self, table):
+        qa_lst = []
+        columns = table['columns']
+        col_name_lst = []
+        for col_info in columns:
+            col_name = col_info['text']
+            col_tokens = col_info['text'].split()
+            if len(col_tokens) > MAX_COL_SIZE:
+                col_name = ' '.join(col_tokens[:MAX_COL_SIZE])
+            col_name_lst.append(col_name) 
+       
+        row_data = table['rows']
+        for row_idx, row_item in enumerate(row_data):
+            cell_lst = row_item['cells']
+            row_info = []
+            for col_idx, cell in enumerate(cell_lst):
+                col_name = col_name_lst[col_idx] 
+                cell_text = cell['text']
+                cell_token_lst = cell_text.split()
+                if len(cell_token_lst) > MAX_ENTITY_SIZE:
+                    cell_text = ' '.join(cell_token_lst[:MAX_ENTITY_SIZE])
+                cell_info = {
+                    'name':col_name,
+                    'value':cell_text
+                }
+                row_info.append(cell_info)
+         
+            questions = self.gen_row_questions(row_info)
+            qa_lst.append(questions)
+            return qa_lst
+         
+    def gen_row_questions(self, row_info):
+        N = len(row_info)
+        M = 3
+        row_question_lst = []
+        for idx_1 in range(0, M):
+            e_s_class = row_info[idx_1]['name'].strip()
+            e_s = row_info[idx_1]['value'].strip()
+            if e_s == '':
+                continue
+            for idx_2 in range(idx_1+1, N):
+                rel = row_info[idx_2]['name'].strip()
+                if rel == '':
+                    continue
+                e_o = row_info[idx_2]['value'].strip()
+                if e_o == '':
+                    continue
+                
+                question_1 = {
+                    'question':'what is the %s of %s %s ?' % (rel, e_s_class, e_s),
+                    'answer':e_o 
+                }
+                question_2 = {
+                    'question':'what is the %s of %s %s ?' % (e_s_class, rel, e_o),
+                    'answer':e_o 
+                }
+
+                row_question_lst.append(question_1)
+                row_question_lst.append(question_2)
+
+        return row_question_lst
