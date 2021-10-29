bd95fdad3e6f3217d84646098af16c5a57aafe0a
mingqiwangcn <mingqiwangcn@gmail.com>
2021-10-29 12:01:53 -0500

    add table file arg

--- table2txt/gen_graph.sh
+++ table2txt/gen_graph.sh
@@ -1,12 +1,13 @@
 if [ "$#" -ne 3 ]; then
-    echo "Usage: ./gen_graph.sh <dataset> <experiment> <strategy>"
+    echo "Usage: ./gen_graph.sh <dataset> <table_file> <experiment>"
     exit
 fi
 dataset=$1
-exptr=$2
-stg=$3
+table_file=$2
+exptr=$3
+stg=CompleteGraph
 python ./table2graph.py \
 --dataset ${dataset} \
---table_file tables_500 \
+--table_file ${table_file} \
 --experiment ${exptr} \
 --strategy ${stg}
