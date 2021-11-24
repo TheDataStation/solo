8b7658e521754a7d94e966a5f6cd09a7125fa4a9
Chameleon Cloud User <mingqiwangcn@gmail.com>
2021-11-23 21:41:44 -0600

    use re

--- table2txt/utils.py
+++ table2txt/utils.py
@@ -15,17 +15,7 @@ def read_table_file(table_lst, data_file, table_filter_set):
 
 def read_template(template_text):
     span_info_lst = []
-   
-    start_tag = '[E'
-    end_tag = '[/E'
-    N = len(template_text)
-    pos_1 = 0
-        pos_2 = pos_1 + len(start_tag)    
-        if template_text[pos_1:pos_2] == start_tag:
-            while pos_2 < N and template_text[pos_2] != ']':
-                pos_2 += 1 
-            ent_idx_text = template_text[(pos_1+2):(pos_2+1) 
-            ent_idx = int(ent_idx_text) 
-            
-            pos_3 = pos_2:
-            while template_text[]
+  
+    start_lst = re.findall(r'\[E\d+\]', template_text)
+    end_lst = re.findall(r'\[/E\d+\]', template_text)
+     
