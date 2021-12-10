0b41cf99575bb60f56a021fd8a7e7b736fffbd11
Chameleon Cloud User <mingqiwangcn@gmail.com>
2021-12-10 19:36:47 +0000

    updatde script args

--- table2txt/gen_graph.sh
+++ table2txt/gen_graph.sh
@@ -1,9 +1,9 @@
-if [ "$#" -ne 3 ]; then
-    echo "Usage: ./gen_graph.sh <dataset> <table_file> <experiment>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./gen_graph.sh <dataset> <experiment>"
     exit
 fi
 dataset=$1
-table_file=$2
+table_file=tables.jsonl
 exptr=$3
 stg=TemplateGraph
 python ./table2graph.py \
