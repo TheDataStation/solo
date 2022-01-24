a571db972f1575f22f728e9f7ad10cbded0261ca
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-01-24 14:31:44 -0600

    gen fusion query

--- gen_simple_fusion_in_decoder_data.py
+++ gen_simple_fusion_in_decoder_data.py
@@ -1,78 +1,40 @@
 import json
 from tqdm import tqdm
-from multiprocessing import Pool as ProcessPool
 import glob
-#from predictor.ar_predictor import ArPredictor
-from fabric_qa.reader.forward_reader.albert.qa_data import data_to_examples
 import torch
 import numpy as np
 
 dataset_name='nq_tables'
 
-retr_result_file = './dataset/nq_tables/bm25_nq_tables_template_graph/dev/preds_dev.json'
-
 def get_qas_data():
-    ret_dict = {}
+    qas_lst = []
     data_file = '/home/cc/data/' + dataset_name + '/interactions/dev_qas.jsonl'
     with open(data_file) as f:
         for line in tqdm(f):
             item = json.loads(line)
-            qid = item['qid']
-            ret_dict[qid] = item
-    return ret_dict
-
-def merge_passages(passage_lst, passage_tags):
-    passage_group_dict = {}
-    for idx, passage in enumerate(passage_lst):
-        table_id = passage_tags[idx]['table_id']
-        if table_id not in passage_group_dict:
-            passage_group_dict[table_id] = []
-        
-        table_passage_lst = passage_group_dict[table_id]
-        table_passage_lst.append(passage)
-
-    merged_passage_lst = []
-    merged_passage_tags = []
-    for table_id in passage_group_dict:
-        table_passage_lst = passage_group_dict[table_id]
-        merged_passage = ' . '.join(table_passage_lst)
-        merged_passage_lst.append(merged_passage)
-        merged_tag = {'table_id':table_id}
-        merged_passage_tags.append(merged_tag)
-
-    return (merged_passage_lst, merged_passage_tags)
+            qas_lst.append(item)
+    return qas_lst
  
 def main():
-    out_data_file = './dataset/nq_tables/bm25_nq_tables_template_graph/dev/fusion_in_decoder_data.jsonl'
+    out_data_file = './fusion_query_dev.json'
     f_o = open(out_data_file, 'w')
-    qas_data_dict = get_qas_data()
-    with open(retr_result_file) as f:
-        for line in tqdm(f):
-            item = json.loads(line)
-            passage_tags = item['passage_tags']
-           
-            out_item = {}
-            out_item['id'] = item['qid']
-            out_item['question'] = item['question']
-            qas_item = qas_data_dict[item['qid']]
-            out_item['table_id_lst'] = qas_item['table_id_lst']
-            out_item['answers'] = qas_item['answers']
-            out_passage_lst = []
-            item_passage_lst = item['passages']
-            
-            merged_passage_lst, merged_passage_tags = merge_passages(item_passage_lst, passage_tags)
+    qas_lst = get_qas_data()
+    for item in tqdm(qas_lst):
+        out_item = {}
+        out_item['id'] = item['qid']
+        out_item['question'] = item['question']
+        out_item['table_id_lst'] = item['table_id_lst']
+        out_item['answers'] = item['answers']
+        out_passage_lst = []
+        
+        out_passage_info = {
+            'title': '',
+            'text': 'This is a example passage.',
+        }
+        out_passage_lst.append(out_passage_info)
 
-            for p_idx, item_passage in enumerate(merged_passage_lst):
-                out_passage = item_passage
-                out_passage_info = {
-                    'title': '',
-                    'text': out_passage,
-                    'tag': merged_passage_tags[p_idx]
-                }
-                out_passage_lst.append(out_passage_info)
-
-            out_item['ctxs'] = out_passage_lst
-            f_o.write(json.dumps(out_item) + '\n')
+        out_item['ctxs'] = out_passage_lst
+        f_o.write(json.dumps(out_item) + '\n')
     
     f_o.close()
 
