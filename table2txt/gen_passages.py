2b8bcfb5664c79f12a6be0c8b679ded6ac8ca0d1
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-01-29 17:53:55 -0600

    update passage dir

--- table2txt/gen_passages.py
+++ table2txt/gen_passages.py
@@ -82,8 +82,7 @@ def main():
     strategy = get_strategy(args.strategy)    
     
     graph_parts_dir = os.path.join('dataset', args.dataset, args.experiment, 'graph_parts')
-    model_out_dir = '/home/cc/code/plms_graph2text/webnlg/outputs/test_model'
-    passage_dir = os.path.join(model_out_dir, args.dataset, args.experiment)
+    passage_dir = os.path.join('dataset', args.dataset, args.experiment, 'generated_passages')
     part_name_lst = sorted(os.listdir(passage_dir))
 
     out_passage_file = os.path.join('dataset', args.dataset, args.experiment, 'graph_passages.json') 
