6746ac006ca31ae15d9da76933621bc2428d4ed8
mingqiwangcn <mingqiwangcn@gmail.com>
2022-01-30 12:20:05 -0600

    table token script

---
+++ table2txt/gen_table_tokens.sh
@@ -0,0 +1,15 @@
+if [ "$#" -ne 3 ]; then
+    echo "Usage: ./gen_table_tokens.sh <dataset> <table_file> <strategy>"
+    exit
+fi
+
+dataset=$1
+table_file=$2
+exptr=$3
+stg=$3
+
+python ./table2tokens.py \
+    --dataset ${dataset} \
+    --table_file ${table_file} \
+    --experiment ${exptr} \
+    --strategy ${stg}
