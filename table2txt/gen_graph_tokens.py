dd58de2d5dfd42212caff060e1113f8bc2ff56a2
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-01-31 19:11:18 -0600

    gen graph tokens

---
+++ table2txt/gen_graph_tokens.py
@@ -0,0 +1,28 @@
+import json
+from tqdm import tqdm
+import os
+
+dataset='nq_tables'
+
+def main():
+    out_file = './dataset/%s/triple_template_graph/graph_tokens.json' % dataset
+    if os.path.exists(out_file):
+        print('[%s] already exists' % out_file)
+        return
+
+    f_o = open(out_file, 'w')
+    delimiters = '   .   '
+    with open('./dataset/%s/triple_template_graph/graph_passages.json' % dataset) as f:
+        for line in tqdm(f):
+            item = json.loads(line)
+            passage = item['passage']
+            text_parts = passage.split(delimiters)
+            assert(len(text_parts) == 3)
+            graph_parts = text_parts[:2]
+            out_text = delimiters.join(graph_parts)
+            item['passage'] = out_text
+            f_o.write(json.dumps(item) + '\n')
+    f_o.close() 
+
+if __name__ == '__main__':
+    main()
