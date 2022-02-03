000e815ca56536fe5daa72872cfbe209f1d4434e
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-02-03 12:21:32 -0600

    update gen_fusion_query

--- table2question/gen_fusion_query.py
+++ table2question/gen_fusion_query.py
@@ -1,6 +1,7 @@
 import json
 import os
 import argparse
+from tqdm import tqdm
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -12,7 +13,7 @@ def get_args():
 def read_meta(meta_file):
     meta_data = []
     with open(meta_file) as f:
-        for line in f:
+        for line in tqdm(f):
             item = json.loads(line)
             meta_data.append(item)
     return meta_data
@@ -20,21 +21,25 @@ def read_meta(meta_file):
 def read_questions(q_file):
     questions = []
     with open(q_file) as f:
-        for line in f:
+        for line in tqdm(f):
             questions.append(line.strip())
     return questions
 
 def main():
     args = get_args()
     data_dir = os.path.join('dataset', args.dataset, args.expr) 
-    meta_file = os.path.join(data_dir, 'meta.jsonl')
+    meta_file = os.path.join(data_dir, 'meta.txt')
     q_file = os.path.join(data_dir, 'questions.txt')
-    out_query_file = os.path.join(data_dir, 'fusion_query.jsonl') 
+    out_query_file = os.path.join(data_dir, 'fusion_query.jsonl')
+    if os.path.exists(out_query_file):
+        print('[%s] already exists' % out_query_file)
+        return 
+     
     f_o_query = open(out_query_file, 'w') 
     meta_data = read_meta(meta_file)
     q_data = read_questions(q_file)
     
-    for idx, question in enumerate(q_data):
+    for idx, question in tqdm(enumerate(q_data)):
         meta_item = meta_data[idx]
         qid = 'q_%d' % idx
         table_id_lst = [meta_item['table_id']]
