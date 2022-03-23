f7908edf36c8bc30d0050adad042c1c3cb93af24
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-03-23 00:12:55 -0500

    ignore long cell text and col name

--- table2txt/graph_strategy/rel_template.py
+++ table2txt/graph_strategy/rel_template.py
@@ -3,6 +3,9 @@ import numpy as np
 import transformers
 from webnlg.data.template_data import TemplateTag
 
+MAX_Cell_Size = 20
+MAX_Rel_Size = 10
+
 class RelationTemplate():
     def __init__(self):
         self.tokenizer = transformers.BertTokenizerFast.from_pretrained('bert-base-uncased')
@@ -29,6 +32,7 @@ class RelationTemplate():
         for col_idx, col_info in enumerate(column_data):
             col_name = col_info['text'].strip()
             rel_name = self.get_rel_name(col_name)
+            rel_size = self.get_token_size(rel_name)
             ent_info_lst = []
             for row_idx, row_info in enumerate(row_data):
                 ent_text = row_info['cells'][col_idx]['text'].strip()
@@ -39,6 +43,7 @@ class RelationTemplate():
             col_entity_info = {
                 'col_name':col_name,
                 'rel_name':rel_name,
+                'rel_size':rel_size,
                 'entities':ent_info_lst
             }
             col_entity_lst.append(col_entity_info) 
@@ -53,9 +58,16 @@ class RelationTemplate():
         column_data = table['columns']
         row_data = table['rows']
         for col_idx, col_info in enumerate(col_entity_lst):
-            rel_name = col_info['rel_name'] 
+            rel_name = col_info['rel_name']
+            rel_size = col_info['rel_size']
+            if rel_size > MAX_Rel_Size: # ignore long col names
+                continue
+             
             obj_info_lst = col_info['entities']
-            sample_space = self.get_sample_space(obj_info_lst) 
+            sample_space = [a for a in obj_info_lst if (a['size'] >= 1) and (a['size'] <= MAX_Cell_Size)]
+            if len(sample_space) == 0: #ignore long cell text
+                continue
+
             M = min(len(sample_space), num_samples)
             sample_objs = random.sample(sample_space, M)
             for obj_info in sample_objs:
@@ -72,15 +84,6 @@ class RelationTemplate():
                 }
                 out_graph_lst.append(graph_info)
         return out_graph_lst
-
-    def get_sample_space(self, data):
-        item_size_lst = [a['size'] for a in data]  
-        q1_pct = np.percentile(item_size_lst, 25)
-        q3_pct = np.percentile(item_size_lst, 75)
-        sample_space = [a for a in data if (a['size'] > q1_pct) and (a['size'] < q3_pct)] 
-        if len(sample_space) == 0:
-            sample_space = data
-        return sample_space
     
     def get_sample_triples(self, col_entity_lst, sub_col_idx, obj_col_idx, num_samples):
         sub_col_data = col_entity_lst[sub_col_idx]
@@ -89,6 +92,10 @@ class RelationTemplate():
         
         obj_col_data = col_entity_lst[obj_col_idx]
         rel_name = obj_col_data['rel_name']
+        rel_size = obj_col_data['rel_size']
+        if rel_size > MAX_Rel_Size:
+            return []
+
         obj_entities = obj_col_data['entities']
         assert(len(sub_entities) == len(obj_entities))
 
@@ -98,22 +105,29 @@ class RelationTemplate():
             assert(sub_ent_item['row'] == obj_ent_item['row'])
             
             sub_text = sub_ent_item['text']
+            sub_size = sub_ent_item['size']
+
             obj_text = obj_ent_item['text']
+            obj_size = obj_ent_item['size']
+
             if sub_text == '' or obj_text == '':
                 continue
+            
+            if sub_size > MAX_Cell_Size or obj_size > MAX_Cell_Size:
+                continue
+
             triple_info = {
                 'sub':(sub_col_name + ' ' + sub_text),
                 'rel':rel_name,
                 'obj':obj_text,
                 'row':sub_ent_item['row'],
                 'sub_col':sub_col_idx,
-                'obj_col':obj_col_idx,
-                'size':(sub_ent_item['size'] + obj_ent_item['size'])
+                'obj_col':obj_col_idx
             }
             triple_info_lst.append(triple_info)
         if len(triple_info_lst) == 0:
             return []
-        sample_space = self.get_sample_space(triple_info_lst)
+        sample_space = triple_info_lst
         M = min(len(sample_space), num_samples)
         sample_triples = random.sample(sample_space, M)
         return sample_triples 
