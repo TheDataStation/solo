cfc9f080f8d108b621f07b0d40439373217995f2
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-04-12 11:49:04 -0500

    add tag script

---
+++ table2txt/tag_retr.sh
@@ -0,0 +1,15 @@
+if [ "$#" -ne 5 ]; then
+    echo "Usage: ./tag_retr_data.py <dataset> <expr> <strategy> <mode> <top_n>"
+    exit
+fi
+dataset=$1
+expr=$2
+strategy=$3
+mode=$4
+top_n=$5
+python ./tag_retr_data.py \
+--dataset ${dataset} \
+--expr ${expr} \
+--strategy ${strategy} \
+--mode ${mode} \
+--top_n ${top_n}
