9a36f068c440ea7603d9900b01c71cabbc0f1eee
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-05-23 19:06:51 -0500

    add num_train arg

--- table2question/gen_sql.sh
+++ table2question/gen_sql.sh
@@ -1,11 +1,13 @@
-if [ "$#" -ne 2 ]; then
-    echo "Usage: ./gen_sql.sh <dataset> <experiment>"
+if [ "$#" -ne 3 ]; then
+    echo "Usage: ./gen_sql.sh <dataset> <experiment> <number of train questions>"
     exit
 fi
 dataset=$1
 table_file=tables.jsonl
 exptr=$2
+num_train=$3
 python ./table2sql.py \
 --dataset ${dataset} \
 --table_file ${table_file} \
 --experiment ${exptr} \
+--num_train_queries ${num_train} 
