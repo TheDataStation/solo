93965d7465804af74f7ba9223788b18f3ae95f28
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-06-20 12:52:32 -0500

    table from csv

---
+++ table_from_csv.py
@@ -0,0 +1,100 @@
+import json
+import csv
+import argparse
+import os
+from tqdm import tqdm
+import uuid
+import glob
+
+def get_out_file(args):
+    data_dir = os.path.join(args.work_dir, 'data/%s/tables' % args.dataset)
+    if not os.path.isdir(data_dir):
+        os.mkdir(data_dir)
+    data_file = os.path.join(data_dir, 'tables.jsonl')
+    return data_file
+
+def read_meta(meta_file):
+    table_title = ''
+    table_id = ''
+    title_prefix = 'title='
+    id_prefix = 'id='
+    if os.path.exists(meta_file):
+        with open(meta_file) as f:
+            for line in f:
+                text = line.strip()
+                if text.startswith(title_prefix):
+                    pos = len(title_prefix)
+                    table_title = text[pos:].strip()
+                elif text.startswith(id_prefix):
+                    pos = len(id_prefix)
+                    table_id = text[pos:].strip()
+    return (table_title, table_id)
+
+def read_table(csv_file, meta_file):
+    file_name = os.path.basename(os.path.splitext(csv_file)[0])
+    table_title, table_id = read_meta(meta_file)
+    if table_title == '':
+        table_title = file_name
+    if table_id == '':
+        table_id = file_name + ' - ' + str(uuid.uuid4())
+    table = {
+        'columns':None,
+        'rows':[],
+        'tableId':table_id,
+        'documentTitle':table_title,
+    }
+    col_name_lst = None
+    with open(csv_file) as f:
+        reader = csv.reader(f, delimiter=',')
+        row_data = table['rows']
+        for row, item in enumerate(reader):
+            if row == 0:
+                col_name_lst = item
+                table['columns'] = [{'text':col_name} for col_name in col_name_lst]
+            else:
+                assert(len(item) == len(col_name_lst))
+                cells = [{'text':a} for a in item]
+                row_data.append(cells)
+    return table
+
+def main(args):
+    out_file = get_out_file(args)
+    if os.path.exists(out_file):
+        msg_text = '(%s) already exists' % out_file
+        msg_info = {
+            'state':False,
+            'msg':msg_text
+        }
+        return msg_info
+
+    f_o = open(out_file, 'w')
+
+    dataset_dir = os.path.join(args.work_dir, 'data', args.dataset)
+    csv_file_pattern = os.path.join(dataset_dir, 'tables_csv', '**', '*.csv')
+    csv_file_lst = glob.glob(csv_file_pattern, recursive=True)
+    for csv_file in tqdm(csv_file_lst):
+        meta_file = os.path.splitext(csv_file)[0] + '.meta'
+        table = read_table(csv_file, meta_file)
+        f_o.write(json.dumps(table) + '\n')
+    
+    f_o.close()
+
+    msg_info = {
+        'state':True,
+    }
+    return msg_info
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--work_dir', type=str)
+    parser.add_argument('--dataset', type=str)
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    args = get_args()
+    msg_info = main(args)
+    if not msg_info['state']:
+        print(msg_info['msg'])
+
+
