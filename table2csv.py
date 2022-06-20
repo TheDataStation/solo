9c7fce735d707d8096fd48dac7befef0c3c3da2b
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-06-19 21:43:09 -0500

    update table file

--- table2csv.py
+++ table2csv.py
@@ -14,25 +14,23 @@ def read_small_table_set(data_file):
     return table_set
 
 def read_table(args):
-    data_file = '/home/cc/data/%s/tables/tables.jsonl' % args.dataset
-    small_table_set = read_small_table_set(args.input_tables)
+    data_file = '/home/cc/code/data/%s/tables/tables.jsonl' % args.dataset
+    #small_table_set = read_small_table_set(args.input_tables)
     with open(data_file) as f:
         for line in f:
             table_data = json.loads(line)
             table_id = table_data['tableId']
-            if table_id not in small_table_set:
-                continue
+            #if table_id not in small_table_set:
+            #    continue
             yield table_data
 
 def main():
     args = get_args()
     table_id_dict = {}
+    table_seq_no = 0
     for table in tqdm(read_table(args)):
-        table_id = table['tableId']
-        if '/' in table_id:
-            table_id = table_id.replace('/', '[left-slash]')
-
-        out_file = os.path.join(args.output_dir, '%s.csv' % table_id)
+        out_file = os.path.join(args.output_dir, '%s.csv' % table_seq_no)
+        table_seq_no += 1
         with open(out_file, 'w') as f_o:
             columns = table['columns']
             writer = csv.writer(f_o)
