5f4b83d9a662217104e2ace30b11acd365e286ea
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-06-24 11:42:11 -0500

    code setup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
-cd ../fusion_in_decoder/
+cd ..
+git clone git@github.com:mingqiwangcn/fusion_in_decoder.git
+cd fusion_in_decoder
 bash prep_env.sh
-cd ../plms_graph2text
+cd ..
+git clone git@github.com:mingqiwangcn/plms_graph2text.git
+cd plms_graph2text
 bash prep_env.sh
 
