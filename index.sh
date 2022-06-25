b8c739314e1c7806417364728da27d49ce500df3
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-06-24 21:54:36 -0500

    add python path

--- index.sh
+++ index.sh
@@ -4,7 +4,9 @@ if [ "$#" -ne 1 ]; then
 fi
 work_dir="$(dirname "$PWD")"
 dataset=$1
+export PYTHONPATH=${work_dir}/fusion_in_decoder:${work_dir}/open_table_discovery:${work_dir}/plms_graph2text
 source ../pyenv/fusion_decoder/bin/activate
+
 python index_tables.py \
 --work_dir ${work_dir} \
 --dataset ${dataset} \
