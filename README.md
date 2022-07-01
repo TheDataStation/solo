6c09ab39c7d7ad60ae06d2070d6a441144e0e25c
Chameleon Cloud User <cc@data-1.novalocal>
2022-07-01 01:49:06 +0000

    update setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Data Discovery using Natural Language Questions via a Self-Supervised Approach
+
--- setup.sh
+++ setup.sh
@@ -1,3 +1,14 @@
+sudo apt-get update
+sudo apt-get -y install libssl-dev libffi-dev zlib1g-dev libsqlite3-dev gcc g++ make sqlite3 liblzma-dev libbz2-dev
+mkdir -p downloads/python_install
+cd downloads/python_install
+wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
+tar -xvzf Python-3.7.9.tgz
+cd Python-3.7.9
+sudo ./configure --enable-loadable-sqlite-extensions
+sudo make install
+rm -rf downloads
+
 cd ..
 git clone git@github.com:mingqiwangcn/fusion_in_decoder.git
 cd fusion_in_decoder
