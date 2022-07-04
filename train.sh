349e839c66b261b8897664ea559e1c40a992a1a0
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-07-04 10:56:37 -0500

    update cuda setting

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: ./train.sh <dataset>"
     exit
 fi
-CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0
 work_dir="$(dirname "$PWD")"
 dataset=$1
 export PYTHONPATH=${work_dir}/fusion_in_decoder:${work_dir}/open_table_discovery:${work_dir}/plms_graph2text
