39c832c6a3a71be289bd3a5131a98e0ec48c9c58
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-07-04 12:25:30 -0500

    add port

--- run_demo.sh
+++ run_demo.sh
@@ -1,6 +1,11 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./run_demo.sh <port>"
+    exit
+fi
+port=$1
 export CUDA_VISIBLE_DEVICES=0
 work_dir="$(dirname "$PWD")"
 export PYTHONPATH=${work_dir}/fusion_in_decoder:${work_dir}/open_table_discovery:${work_dir}/plms_graph2text
 source ../pyenv/fusion_decoder/bin/activate
 python -m ipykernel install --user --name fusion_decoder
-jupyter notebook --no-browser --port=8080 
+jupyter notebook --no-browser --port=${port}
