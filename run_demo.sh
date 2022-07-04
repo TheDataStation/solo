eb08dda13ee33b7b02bfab30b7c9f8483508300e
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-07-04 12:19:52 -0500

    add demo script

---
+++ run_demo.sh
@@ -0,0 +1,6 @@
+export CUDA_VISIBLE_DEVICES=0
+work_dir="$(dirname "$PWD")"
+export PYTHONPATH=${work_dir}/fusion_in_decoder:${work_dir}/open_table_discovery:${work_dir}/plms_graph2text
+source ../pyenv/fusion_decoder/bin/activate
+python -m ipykernel install --user --name fusion_decoder
+jupyter notebook --no-browser --port=8080 
