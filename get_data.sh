1fa5dbae20d44cb317903f4801f36a5b421e6e55
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-07-06 16:12:25 -0500

    update download scripts

--- get_data.sh
+++ get_data.sh
@@ -5,8 +5,9 @@ fi
 dataset=$1
 work_dir="$(dirname "$PWD")"
 data_dir=${work_dir}/data
-mkdir ${data_dir}
+mkdir -p ${data_dir}
 cd ${data_dir}
+rm -f ${dataset}.tar
 wget http://plover.cs.uchicago.edu/open_table_discovery/${dataset}.tar
 rm -rf ${dataset}
 tar -xf ${dataset}.tar
@@ -18,8 +19,11 @@ index_dir=on_disk_index_${dataset}_rel_graph
 rm -rf ${index_dir}
 mkdir ${index_dir}
 cd ${index_dir}
+rm -f merged_index.ivfdata
 wget http://plover.cs.uchicago.edu/open_table_discovery/${index_dir}/merged_index.ivfdata
+rm -f passages.jsonl
 wget http://plover.cs.uchicago.edu/open_table_discovery/${index_dir}/passages.jsonl
+rm -f populated.index
 wget http://plover.cs.uchicago.edu/open_table_discovery/${index_dir}/populated.index
 
 
--- get_models.sh
+++ get_models.sh
@@ -1,6 +1,6 @@
 work_dir="$(dirname "$PWD")"
 cd ${work_dir}
-rm -rf models
+rm -f models.tar
 wget http://plover.cs.uchicago.edu/open_table_discovery/models.tar
 tar -xf models.tar
-rm -rf models.tar
+rm -f models.tar
