b70460c9066bc42c83e0926841c8eef02e86aaef
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-10-11 04:50:29 +0000

    add arg

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-if [ "$#" -ne 3 ]; then
-    echo "Usage: ./test.sh <dataset> <bnn> <model_dir>"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: ./test.sh <dataset> <bnn> <model_dir> <table_repre>"
     exit
 fi
 export CUDA_VISIBLE_DEVICES=0
@@ -7,11 +7,12 @@ work_dir="$(dirname "$PWD")"
 dataset=$1
 bnn=$2
 train_model_dir=~/code/fusion_in_decoder/output/$3
+table_repre=$4
 export PYTHONPATH=${work_dir}/fusion_in_decoder:${work_dir}/open_table_discovery:${work_dir}/plms_graph2text
 source ../pyenv/fusion_decoder/bin/activate
 python tester.py \
     --work_dir ${work_dir} \
     --dataset ${dataset} \
     --bnn ${bnn} \
-    --train_model_dir ${train_model_dir} 
-
+    --train_model_dir ${train_model_dir} \
+    --table_repre ${table_repre} \
