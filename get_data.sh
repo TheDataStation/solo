96d91b48dbaf67baf72b4b64606b8c4220de2bd9
Chameleon Cloud User <mingqiwangcn@gmail.com>
2022-10-21 16:11:31 -0500

    update server

--- get_data.sh
+++ get_data.sh
@@ -1,15 +1,19 @@
-if [ "$#" -ne 2 ]; then
-  echo "Usage: ./get_data.sh <dataset> <server>"
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ./get_data.sh <dataset>"
   exit
 fi
 dataset=$1
+if [ "${dataset}" != "fetaqa" ]; then
+    echo "only fetaqa supported right now, please use ./get_data.sh fetaqa"
+    exit
+fi
 server=$2
 work_dir="$(dirname "$PWD")"
 data_dir=${work_dir}/data
 mkdir -p ${data_dir}
 cd ${data_dir}
 rm -f ${dataset}.tar
-wget http://${server}/open_table_discovery/${dataset}.tar
+wget https://storage.googleapis.com/open_data_123/fetaqa_data/fetaqa.tar
 rm -rf ${dataset}
 tar -xf ${dataset}.tar
 rm ${dataset}.tar
@@ -21,10 +25,9 @@ rm -rf ${index_dir}
 mkdir ${index_dir}
 cd ${index_dir}
 rm -f merged_index.ivfdata
-wget http://${server}/open_table_discovery/${index_dir}/merged_index.ivfdata
+wget https://storage.googleapis.com/open_data_123/${dataset}_data/index/merged_index.ivfdata
 rm -f passages.jsonl
-wget http://${server}/open_table_discovery/${index_dir}/passages.jsonl
+wget https://storage.googleapis.com/open_data_123/${dataset}_data/index/passages.jsonl
 rm -f populated.index
-wget http://${server}/open_table_discovery/${index_dir}/populated.index
-
+wget https://storage.googleapis.com/open_data_123/${dataset}_data/index/populated.index 
 
--- get_models.sh
+++ get_models.sh
@@ -1,11 +1,6 @@
-if [ "$#" -ne 1 ]; then
-  echo "Usage: ./get_models.sh <server>"
-  exit
-fi
 work_dir="$(dirname "$PWD")"
-server=$1
 cd ${work_dir}
 rm -f models.tar
-wget http://${server}/open_table_discovery/models.tar
+wget https://storage.googleapis.com/open_data_123/models.tar
 tar -xf models.tar
 rm -f models.tar
