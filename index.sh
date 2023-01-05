95ed4d44b98160a144ad5ca132967157e55ccdea
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-01-05 11:54:51 -0600

    update PYTHONPATH

--- index.sh
+++ index.sh
@@ -4,8 +4,8 @@ if [ "$#" -ne 1 ]; then
 fi
 work_dir="$(dirname "$PWD")"
 dataset=$1
-export PYTHONPATH=${work_dir}/fusion_in_decoder:${work_dir}/open_table_discovery:${work_dir}/plms_graph2text
-source ../pyenv/fusion_decoder/bin/activate
+export PYTHONPATH=${work_dir}/open_table_discovery:${work_dir}/open_table_discovery/relevance:${work_dir}/open_table_discovery/sql2question
+source ../pyenv/s2ld/bin/activate
 
 python index_tables.py \
 --work_dir ${work_dir} \
