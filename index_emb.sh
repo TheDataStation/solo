8826bb99a995a622932394ba80747c93f2e4cae2
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-01-26 18:26:33 -0600

    add pipe step script

---
+++ index_emb.sh
@@ -0,0 +1,13 @@
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ./index_emb.sh <dataset>"
+  exit
+fi
+work_dir="$(dirname "$PWD")"
+dataset=$1
+export PYTHONPATH=${work_dir}/open_table_discovery:${work_dir}/open_table_discovery/relevance:${work_dir}/open_table_discovery/sql2question
+source ../pyenv/s2ld/bin/activate
+
+python index_tables.py \
+--work_dir ${work_dir} \
+--dataset ${dataset} \
+--pipe_step emb_to_index \
