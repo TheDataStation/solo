aa912169800633b5646e1802b6a0911d65e3e3da
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-03-09 17:27:49 -0600

    add args

--- relevance/train_student.sh
+++ relevance/train_student.sh
@@ -1,10 +1,15 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./train_student.sh <out_dir>"
+    exit
+fi
+
 python train_student_retriever.py \
-        --lr 1e-4 \
-        --optim adamw \
-        --scheduler linear \
-        --train_data train_data.json \
-        --eval_data eval_data.json \
-        --n_context 100 \
-        --total_steps 20000 \
-        --scheduler_steps 30000 \
+--lr 1e-4 \
+--optim adamw \
+--scheduler linear \
+--train_data train_data.json \
+--eval_data eval_data.json \
+--n_context 100 \
+--total_steps 20000 \
+--scheduler_steps 30000 \
 
