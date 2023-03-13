417374cc7e7e436fe968e01866523da4407bd1e6
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-03-13 00:22:23 -0500

    update data file

--- relevance/train_student.sh
+++ relevance/train_student.sh
@@ -4,8 +4,8 @@ python train_student_retriever.py \
 --optim adamw \
 --scheduler linear \
 --teacher_model_path /home/cc/code/table_discovery_project/models/tqa_retriever \
---train_data ./data/trivia_qa/train_examples.jsonl \
---eval_data ./data/trivia_qa/dev_examples.jsonl \
+--train_data ./data/trivia_qa/train.jsonl \
+--eval_data ./data/trivia_qa/dev.jsonl \
 --n_context 100 \
 --total_steps 20000 \
 --scheduler_steps 30000 \
