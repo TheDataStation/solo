c77c66a5b157d843722128af2200d02bd910e691
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-03-17 11:08:13 -0500

    update model file

--- relevance/retr_flat.sh
+++ relevance/retr_flat.sh
@@ -1,7 +1,8 @@
 python passage_retrieval.py \
+ --is_student 1 \
  --data ./data/TQA/test.json \
  --passages ./data/wiki.jsonl \
- --passages_embeddings ./data/wiki_embeddings_float16_00 \
- --model_path ~/code/table_discovery_project/models/tqa_retriever \
- --output_path output/retr_teacher_float_16.json \
+ --passages_embeddings "./data/wiki_embeddings_student_float16/embeddings*" \
+ --model_path ~/code/table_discovery_project/models/student_tqa_retriever_epoch_16 \
+ --output_path output/retr_student_float_16.json \
 
