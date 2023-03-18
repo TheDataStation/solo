f16434db6ab0124691bef8e9704d6330a71b98c7
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-03-18 00:46:47 -0500

    update prep script

--- prep_env.sh
+++ prep_env.sh
@@ -1,5 +1,16 @@
-python3.7 -m venv ../pyenv/s2ld
-source ../pyenv/s2ld/bin/activate
-pip install -r ./requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
+conda create -y --name s2ld python=3.7.9
+conda activate s2ld
+conda install -y -c pytorch faiss-gpu
+pip install --upgrade pip
+pip install -r ./requirements.txt
+pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
+sudo apt update
+sudo apt install -y wget gcc-8 unzip libssl1.0.0 software-properties-common
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt update
+sudo apt-get install -y --only-upgrade libstdc++6
+
 cd ./relevance/transformers-3.0.2
 pip install -e .
+cd ../../
+
