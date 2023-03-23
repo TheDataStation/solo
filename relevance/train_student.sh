27d642527d39831564ad741e2e544047ceef3565
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-03-22 21:08:02 -0500

    update train file

--- relevance/train_student.sh
+++ relevance/train_student.sh
@@ -4,8 +4,8 @@ python train_student_retriever.py \
 --optim adamw \
 --scheduler linear \
 --teacher_model_path /home/cc/code/table_discovery_project/models/tqa_retriever \
---train_data ./data/trivia_qa/train_0.jsonl \
---eval_data ./data/trivia_qa/dev_0.jsonl \
+--train_data ./data/trivia_qa/train.jsonl \
+--eval_data ./data/trivia_qa/dev.jsonl \
 --per_gpu_batch_size 32 \
 --n_context 100 \
 --total_steps 20000 \
