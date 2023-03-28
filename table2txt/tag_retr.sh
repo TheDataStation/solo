480d5b4e2e3a3b626896cfa281d86fe32845a2b9
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-03-27 23:21:12 -0500

    update arg

--- table2txt/tag_retr.sh
+++ table2txt/tag_retr.sh
@@ -1,15 +1,19 @@
-if [ "$#" -ne 5 ]; then
-    echo "Usage: ./tag_retr_data.py <dataset> <table_expr> <strategy> <mode> <synthetic>"
+if [ "$#" -ne 6 ]; then
+    echo "Usage: ./tag_retr_data.py <dataset> <table_expr> <strategy> <mode> <top_n> <synthetic>"
     exit
 fi
 dataset=$1
 table_expr=$2
 strategy=$3
 mode=$4
-synthetic=$5
+top_n=$5
+synthetic=$6
 python ./tag_retr_data.py \
 --dataset ${dataset} \
 --table_expr ${table_expr} \
 --strategy ${strategy} \
 --mode ${mode} \
+--top_n ${top_n} \
+--min_tables 5 \
 --synthetic ${synthetic} \
+
