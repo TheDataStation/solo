b89df67097e5b60c6841140572d3b2d35745cb7f
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-04-12 23:48:32 -0500

    fix msg

--- relevance/src/ondisk_index.py
+++ relevance/src/ondisk_index.py
@@ -266,6 +266,6 @@ def get_args():
 if __name__ == '__main__':
     args = get_args()
     msg_info = main(args)
-    if not mag_info['state']:
+    if not msg_info['state']:
         print(msg_info['msg']) 
     
