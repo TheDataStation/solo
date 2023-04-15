e4ce55cc472bc152608fd2cb2aa704e04203bc5b
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-04-15 16:18:52 -0500

    add server script

---
+++ run_server.sh
@@ -0,0 +1,12 @@
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./run_server.sh <dataset>"
+    exit
+fi
+dataset=$1
+work_dir="$(dirname "$PWD")"
+#ln -s ${work_dir}/index data
+#ln -s ${work_dir}/open_table_discovery/system.config
+export PYTHONPATH=${work_dir}/open_table_discovery:${work_dir}/open_table_discovery/relevance:${work_dir}/open_table_discovery/sql2question
+source ../pyenv/s2ld/bin/activate
+python ./demo/app.py --work_dir ${work_dir} --dataset ${dataset}
+
