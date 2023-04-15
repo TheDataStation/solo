95d89ecc26fbecf011c34470ab8c497a45335890
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-04-15 16:47:41 -0500

    update server script

--- run_server.sh
+++ run_server.sh
@@ -7,6 +7,5 @@ work_dir="$(dirname "$PWD")"
 #ln -s ${work_dir}/index data
 #ln -s ${work_dir}/open_table_discovery/system.config
 export PYTHONPATH=${work_dir}/open_table_discovery:${work_dir}/open_table_discovery/relevance:${work_dir}/open_table_discovery/sql2question
-source ../pyenv/s2ld/bin/activate
 python ./demo/app.py --work_dir ${work_dir} --dataset ${dataset}
 
