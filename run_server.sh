d66a124c80315c4ac8a64e5d5c12245fd6cea9fa
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-04-15 16:59:20 -0500

    update script

--- run_server.sh
+++ run_server.sh
@@ -4,8 +4,6 @@ if [ "$#" -ne 1 ]; then
 fi
 dataset=$1
 work_dir="$(dirname "$PWD")"
-#ln -s ${work_dir}/index data
-#ln -s ${work_dir}/open_table_discovery/system.config
 export PYTHONPATH=${work_dir}/open_table_discovery:${work_dir}/open_table_discovery/relevance:${work_dir}/open_table_discovery/sql2question
 python ./demo/app.py --work_dir ${work_dir} --dataset ${dataset}
 
