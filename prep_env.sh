0efc0cbde63cdd4e37c3dbfd3d14e4c91c8ce93b
Qiming Wang <mingqiwangcn@gmail.com>
2023-10-05 17:03:49 -0500

    remove sudo cmd and update encode batch size

--- prep_env.sh
+++ prep_env.sh
@@ -5,11 +5,6 @@ conda install -y -c pytorch faiss-gpu
 pip install --upgrade pip
 pip install -r ./requirements.txt
 pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
-sudo apt update
-sudo apt install -y wget gcc-8 unzip libssl1.1 software-properties-common
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-sudo apt update
-sudo apt-get install -y --only-upgrade libstdc++6
 
 cd ./relevance/transformers-3.0.2
 pip install -e .
--- setup.sh
+++ setup.sh
@@ -1,17 +1,5 @@
-sudo apt-get update
-sudo apt-get -y install libssl-dev libffi-dev zlib1g-dev libsqlite3-dev gcc g++ make sqlite3 liblzma-dev libbz2-dev
-mkdir -p downloads/python_install
 cur_dir=${PWD}
 work_dir="$(dirname "$PWD")"
-cd downloads/python_install
-wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
-tar -xvzf Python-3.7.9.tgz
-cd Python-3.7.9
-sudo ./configure --enable-loadable-sqlite-extensions
-sudo make install
-cd ${cur_dir}
-sudo rm -rf downloads
-
 source prep_env.sh
 cd ${cur_dir}/sql2question
 bash prep_env.sh
@@ -19,3 +7,4 @@ cd ${work_dir}
 mkdir -p index
 cd ${cur_dir}
 ln -s ${work_dir}/index data
+ln -s ${work_dir}/solo open_table_discovery
