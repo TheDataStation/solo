0a280efd7179d0fbeeabfb3d998d6b5b512e2d9f
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-10-11 23:11:52 -0500

    use conda instead of pip

--- prep_env.sh
+++ prep_env.sh
@@ -2,9 +2,8 @@ eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 conda create -y --name s2ld python=3.7.9
 conda activate s2ld
 conda install -y -c pytorch faiss-gpu
-pip install --upgrade pip
-pip install -r ./requirements.txt
-pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113
+conda install -y --file ./requirements.txt
+conda install -y pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch
 
 cd ./relevance/transformers-3.0.2
 pip install -e .
