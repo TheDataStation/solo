91d14a6e7720713646ff04344cb2f4fed475d5cb
Chameleon Cloud User <mingqiwangcn@gmail.com>
2023-11-04 17:05:59 -0500

    update cuda device use

--- index.sh
+++ index.sh
@@ -2,6 +2,7 @@ if [ "$#" -ne 1 ]; then
   echo "Usage: ./index.sh <dataset>"
   exit
 fi
+export CUDA_VISIBLE_DEVICES=0
 work_dir="$(dirname "$PWD")"
 dataset=$1
 export PYTHONPATH=${work_dir}/open_table_discovery:${work_dir}/open_table_discovery/relevance:${work_dir}/open_table_discovery/sql2question
